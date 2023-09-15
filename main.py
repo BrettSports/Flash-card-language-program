7decffe0d6bd4f6133ee4ca800f0f99f5a6a5eb0
Brett Sports <omnipotentshoemaker@gmail.com>
2023-09-15 00:17:49 -0400

    Updated the buttons so that the check button allows you to remove the word from the list and save the edited list in a new csv

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 from tkinter import *
-from tkinter import messagebox
+
+import pandas
 import pandas as pd
-import os
 import random
 
 
 BACKGROUND_COLOR = "#B1DDC6"
-# IMAGE_FOLDER = os.path.join(os.path.dirname(__file__), "images")
+current_card = {}
+to_learn = {}
+
+
+try:
+    data = pd.read_csv("data/spanish_words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pandas.read_csv("data/spanish_words.csv")
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
 
 # ----------------------------  FUNCTIONS ------------------------------- #
-data = pd.read_csv("data/spanish_words.csv")
-to_learn = data.to_dict(orient="records")
-current_card = {}
 def next_card():
     global current_card, flip_timer
     window.after_cancel(flip_timer)
@@ -19,7 +26,18 @@ def next_card():
     canvas.itemconfig(card_title, text="Spanish", fill="black")
     canvas.itemconfig(card_word, text=current_card["Spanish"], fill="black")
     canvas.itemconfig(card_background, image=card_front_img)
-    window.after(3000, func=flip_card)
+    window.after(4000, func=flip_card)
+
+def is_known():
+    to_learn.remove(current_card)
+    # print(len(to_learn))
+    try:
+        data = pandas.DataFrame(to_learn)
+        data.to_csv("data/spanish_words_to_learn.csv", index=False)
+    except Exception as e:
+        print(f"Error saving data to CSV: {str(e)}")
+    next_card()
+
 
 def flip_card():
     canvas.itemconfig(card_title, text="English", fill="white")
@@ -28,12 +46,13 @@ def flip_card():
 
 # ---------------------------- UI SETUP ------------------------------- #
 
+# Window Setup
 window = Tk()
 window.title("Brett Sports Language Learny Thang")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
-flip_timer = window.after(3000, func=flip_card)
+flip_timer = window.after(4000, func=flip_card)
 
-# Card
+# Card Image Setup
 canvas = Canvas(width=800, height=526, highlightthickness=0)
 card_front_img = PhotoImage(file="images/card_front.png")
 card_back_img = PhotoImage(file="images/card_back.png")
@@ -50,7 +69,7 @@ unknown_button = Button(image=cross_image, highlightthickness=0, command=next_ca
 unknown_button.grid(column=0, row=1)
 
 check_image = PhotoImage(file="images/right.png")
-check_button = Button(image=check_image, highlightthickness=0, command=next_card)
+check_button = Button(image=check_image, highlightthickness=0, command=is_known)
 check_button.grid(column=1, row=1)
 
 next_card()
